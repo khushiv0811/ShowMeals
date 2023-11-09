21e72bedb86c14c8396b26bc552b792dff97146b
khushiv0811 <65448570+khushiv0811@users.noreply.github.com>
2023-11-09 16:02:48 -0500

    Initial Commit

---
+++ ShowMeals/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ShowMeals
+//
+//  Created by Kushboo Patel on 11/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShowMeals/ShowMealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowMealsApp.swift
+//  ShowMeals
+//
+//  Created by Kushboo Patel on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ShowMealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
