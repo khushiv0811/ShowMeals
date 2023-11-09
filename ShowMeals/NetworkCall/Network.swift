65e552ff434bc5c43ae35d402bdef9c236eb1daa
khushiv0811 <65448570+khushiv0811@users.noreply.github.com>
2023-11-09 17:49:41 -0500

    Add network call to get Meal Details

--- ShowMeals/NetworkCall/Network.swift
+++ ShowMeals/NetworkCall/Network.swift
@@ -18,5 +18,15 @@ func fetchMeals() -> some Publisher<Meals, Error> {
         .shared
         .dataTaskPublisher(for: url)
         .map(\.data)
+        .print()
         .decode(type: Meals.self, decoder: jsonDecoder)
+}
+
+func fetchMealDetails(meal: Meal) -> some Publisher<MealDetail, Error> {
+    let url = URL(string: "https://themealdb.com/api/json/v1/1/lookup.php?i=\(meal.id)")!
+    return URLSession
+        .shared
+        .dataTaskPublisher(for: url)
+        .map(\.data)
+        .decode(type: MealDetail.self, decoder: jsonDecoder)
 }
