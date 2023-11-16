d22b148e28c1276071515512956a35df58dd395b
khushiv0811 <65448570+khushiv0811@users.noreply.github.com>
2023-11-16 16:41:01 -0500

    Add unit tests for network calls

---
+++ ShowMealsTests/MockNetworkCall.swift
@@ -0,0 +1,56 @@
+//
+//  MockNetworkCall.swift
+//  ShowMealsTests
+//
+//  Created by Kushboo Patel on 11/16/23.
+//
+
+import XCTest
+import Combine
+@testable import ShowMeals
+
+
+class NetworkTests: XCTestCase {
+    func testFetchMeals() {
+        
+        let expectation = XCTestExpectation(description: "Meals fetched successfully")
+        
+        _ = fetchMeals()
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .finished:
+                        expectation.fulfill()
+                    case .failure(let error):
+                        XCTFail("Failed to fetch meals with error: \(error)")
+                    }
+                },
+                receiveValue: { mealsResponse in
+                    // Perform assertions on the mealsResponse if needed
+                }
+            )
+    }
+    
+    func testFetchMealDetails() {
+        
+        let meal = Meal(id: "123", name: "TestMeal", mealImage: "test.jpg")
+        let expectation = XCTestExpectation(description: "Meal details fetched successfully")
+        
+        _ = fetchMealDetails(meal: meal)
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .finished:
+                        expectation.fulfill()
+                    case .failure(let error):
+                        XCTFail("Failed to fetch meal details with error: \(error)")
+                    }
+                },
+                receiveValue: { mealDetailResponse in
+                    // Perform assertions on the mealDetailResponse if needed
+                }
+            )
+    }
+}
+
+
